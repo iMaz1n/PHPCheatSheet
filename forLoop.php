991afd1dae5d2058d7263d8a9a4677e9f166c789
iMaz1n <ilzzli@hotmail.com>
2019-11-01 21:36:24 +0300

    Create forLoop.php

---
+++ forLoop.php
@@ -0,0 +1,20 @@
+<?php
+
+//
+//  forLoop.php
+//  for loop example in php
+//
+//  Created by Mazen Hrazi on 01/11/19 (Twitter: @iMaz1n).
+//  Copyright © 2019 iZONA. All rights reserved.
+//
+
+for ($x = 0; $x < 5; $x++) {
+	echo "Hello ", $x+1 , "\n";
+	printf("Welcome %u\n",$x+1);
+}
+?>
+<!-- loop with html -->
+<?php for ($x = 0; $x < 5; $x++) { ?>
+<p>Loop<?=$x+1?></p>
+<button type="button">Click <?=$x+1?></button>
+<?php }?>
