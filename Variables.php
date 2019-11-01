d81666d1a2e07d9cbe1a0c20a45348cde2e8ace2
iMazn <ilzzli@hotmail.com>
2019-11-01 15:36:39 +0300

    add Variables

---
+++ Variables.php
@@ -0,0 +1,23 @@
+<?php
+
+//
+//  Variables.php
+//  Variables example in php
+//
+//  Created by Mazen Hrazi on 01/11/19 (Twitter: @iMaz1n).
+//  Copyright © 2019 iZONA. All rights reserved.
+//
+
+	$int = 22;
+	$str = "Hello from Mazen";
+	$isProgrammer = true;
+	$pi = 3.14;
+	$int = $int-1;
+	$int--;
+	
+	echo "The int is: ". $int . " and bool : " . $isProgrammer . " Pi: " . $pi;
+	echo "\n";
+	echo "from echo with $pi";
+	echo "\n";
+	printf("$str I'm %u",$int);
+?>
